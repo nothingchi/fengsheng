a9c09e15089e9aa34358fbc05bd6596314daa3c5
colinchi <chimingyu_neu@163.com>
2022-05-13 23:21:08 +0800

    add ver 0.1

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -9,13 +9,72 @@ import xmltodict
 import time
 import random
 
-roles = [1, 100, 9]
-@app.route('/')
+numPlayerConfig = [
+[], #1
+[], #2
+[], #3
+[], #4
+[], #5
+[], #6
+[], #7
+[5, 1, 1, 1], #8
+[5, 2, 1, 1], #9
+[7, 0, 2, 1], #10
+[7, 1, 2, 1], #11
+[7, 2, 2, 1], #12
+[9, 0, 3, 1], #13
+[9, 1, 3, 1], #14
+[9, 2, 3, 1] #15
+]
+
+roles = {
+"villagers": ["洗衣妇人", "图书管理员", "调查员", "厨师", "僧侣", "共情者", "占卜师", "守鸦人", "士兵", "市长", "杀手", "掘墓人", "圣女"],
+"outsider": ["圣徒", "酒鬼", "隐士", "管家"],
+"minions": ["荡妇", "男爵", "间谍", "下毒者"],
+"demon": ["小恶魔"]
+}
+@app.route('/', methods=['POST'])
 def index():
     """
     :return: 返回index页面
     """
-    return render_template('index.html')
+    if request.method == "POST":
+        form_data = request.form
+        NumPlayer = int(form_data.get("NumPlayer"))
+        results = []
+        if NumPlayer > 15 or NumPlayer < 8:
+            return "fake news"
+        NumIndex = NumPlayer - 1
+        numConfig = numPlayerConfig[NumIndex]
+        villagersNum = numConfig[0]
+        outsiderNum = numConfig[1]
+        minionsNum = numConfig[2]
+        demonNum = numConfig[3]
+        # 首先抽取爪牙看是否有男爵
+        random.shuffle(roles["minions"])
+        minions = roles["minions"][0: minionsNum]
+        if "男爵" in minions:
+            villagersNum -= 2
+            outsiderNum += 2
+        # 抽取外来者看是否有酒鬼
+        random.shuffle(roles["outsider"])
+        outsider = roles["outsider"][0: outsiderNum]
+        if "酒鬼" in outsider:
+            villagersNum += 1 #内置一张酒鬼牌
+        random.shuffle(roles["villagers"])
+        villagers = roles["villagers"][0: villagersNum]
+
+        for role in villagers:
+            results.append(("村民", role))
+        for role in outsider:
+            if "酒鬼" == role:
+                continue
+            results.append(("外来者", role))
+        for role in minions:
+            results.append(("爪牙", role))
+        results.append(("恶魔", "小恶魔"))
+
+    return render_template('index.html', results=results)
 
 
 @app.route('/api/count', methods=['POST'])
@@ -26,7 +85,8 @@ def count():
 
     # 获取请求体参数
     params = request.get_json()
-
+    print(request)
+    print(params) 
     # 检查action参数
     if 'action' not in params:
         return make_err_response('缺少action参数')
